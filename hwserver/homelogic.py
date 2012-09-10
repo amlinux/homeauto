956d89ec2e1be3263b3951e8b81c67ce3cdc680f
Alexander Lourier <aml@rulezz.ru>
2012-09-10 20:03:46 +0000

    Parse button events, switch relays on/off

--- hwserver/homelogic.py
+++ hwserver/homelogic.py
@@ -12,7 +12,24 @@ class HomeLogic(object):
     def __init__(self, dispatcher):
         self.dispatcher = dispatcher
         self.logic = EventReceiver(self)
-        dispatcher.subscribe({}, self.logic)
+        self.relayState = {}
+        self.dispatcher.relay_clear_all()
+        self.dispatcher.subscribe({}, self.logic)
 
     def event_recv(self, event):
-        print "Received packet: %s" % event
+        print event
+        data = event["data"]
+        cmd = data.pop(0)
+        if cmd == ord('B'):
+            btnCmd = data.pop(0)
+            if btnCmd == 1:
+                btn = data.pop(0)
+                print "Pressed button %d" % btn
+                relay = btn + 1
+                newState = not self.relayState.get(relay)
+                self.relayState[relay] = newState
+                self.dispatcher.relay_set(relay, newState)
+            elif btnCmd == 0:
+                btn = data.pop(0)
+                print "Released button %d" % btn
+
